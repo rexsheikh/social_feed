3f8f1fc66d3ea614377502479ccd7ed749295f29
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-04-06 12:46:27 -0400

    completed button functionality so that like/dislike can toggle individually and they are turned inactive when the opposite is clicked.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,50 @@
-import CreatePost from "./CreatePost";
+import React, { useState } from 'react';
+import thumbs_down from "../assets/thumbs_down.png"
+import thumbs_up from "../assets/thumbs_up.png"
+
+
 const Post = (props) => {
+    const [greenButtonClass,setGreenButtonClass] = useState("inactive")
+    const [redButtonClass,setRedButtonClass] = useState("inactive")
+
+    function handleGreenClick(){
+        if(greenButtonClass === "inactive" && redButtonClass === "inactive"){
+            setGreenButtonClass("green-click");
+        }else if(greenButtonClass === "inactive" && redButtonClass === "red-click"){
+            setGreenButtonClass("green-click");
+            setRedButtonClass("inactive");
+        }
+        else{
+            setGreenButtonClass("inactive")
+        }
+    }
+
+    function handleRedClick(){
+        if(greenButtonClass === "inactive" && redButtonClass === "inactive"){
+            setRedButtonClass("red-click")
+        }else if(greenButtonClass === "green-click" && redButtonClass === "inactive"){
+            setGreenButtonClass("inactive");
+            setRedButtonClass("red-click");
+        }
+        else{
+            setRedButtonClass("inactive")
+        }
+    }
+
+
+
     return ( 
         <div>
-            <h2> {props.name} {props.post} </h2>
-            <p> like and dislike buttons</p>
+            <p> {props.name}</p>
+            <p> {props.post}</p>
+            <div className = "image-container"> 
+                <button className= {greenButtonClass} onClick = {handleGreenClick}> 
+                    <img src= {thumbs_up} alt= "thumbs_up"/>
+                </button>
+                <button className={redButtonClass} onClick = {handleRedClick}>
+                 <img src={thumbs_down} alt= "thumbs_down" />
+                </button>
+            </div>
         </div>
         
      );
